dbac0d6da209e78739b4d992cc48d295dd4300fb
kewuaa <202321011111@std.uestc.edu.cn>
2024-12-08 15:16:14 +0800

    feat: more kind of snippets

--- lua/compl/init.lua
+++ lua/compl/init.lua
@@ -493,10 +493,23 @@ function M._on_completedone()
 	end
 
 	-- Expand snippets
-	if kind == "Snippet" then
+	local snip_body = (function()
+		local snip_body
+		if
+			kind:match("Snippet")
+			or completion_item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet
+		then
+			local new_text = vim.tbl_get(completion_item, "textEdit", "newText")
+			local insert_text = completion_item.insertText
+			snip_body = (new_text and new_text:find("%$") and new_text)
+				or (insert_text and insert_text:find("%$") and insert_text)
+		end
+		return snip_body
+	end)()
+	if snip_body then
 		pcall(vim.api.nvim_buf_set_text, bufnr, row - 1, col - vim.fn.strwidth(completed_word), row - 1, col, { "" })
 		pcall(vim.api.nvim_win_set_cursor, winnr, { row, col - vim.fn.strwidth(completed_word) })
-		vim.snippet.expand(vim.tbl_get(completion_item, "textEdit", "newText") or completion_item.insertText or "")
+		vim.snippet.expand(snip_body)
 	end
 
 	-- Apply additionalTextEdits
