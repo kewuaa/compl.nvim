def99649d57180b571921ffb0ecc276ca7b71006
kewuaa <202321011111@std.uestc.edu.cn>
2024-12-08 21:39:36 +0800

    add parse_snippet_body to util.lua

--- lua/compl/util.lua
+++ lua/compl/util.lua
@@ -14,6 +14,21 @@ M.sep = (function()
 	end
 end)()
 
+
+function M.parse_snippet_body(kind, completion_item)
+	local snip_body
+	if
+		kind:match("Snippet")
+		or completion_item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet
+	then
+		local new_text = vim.tbl_get(completion_item, "textEdit", "newText")
+		local insert_text = completion_item.insertText
+		snip_body = (new_text and new_text:find("%$") and new_text)
+		or (insert_text and insert_text:find("%$") and insert_text)
+	end
+	return snip_body
+end
+
 function M.debounce(timer, timeout, callback)
 	return function(...)
 		local argv = { ... }
